aa8d473085a6ec726a8c70663c23178fdbd241e6
JCruz6725 <john.cruz6725@gmail.com>
2025-09-19 11:44:42 -0500

    Add project files.

---
+++ TaskManager_ConsoleApp_Terry/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManager_ConsoleApp_Terry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
